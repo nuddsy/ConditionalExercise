fea68b478013d7a3e8e51e8daf95fe1e29b54e45
Matt Nudds <matthewnudds1@gmail.com>
2024-10-24 10:13:50 +0100

    ADD: Completed 1.3

--- if_switch_calc/src/Main.java
+++ if_switch_calc/src/Main.java
@@ -1,17 +1,32 @@
+import java.util.Scanner;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        Scanner scanner = new Scanner(System.in);
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+        System.out.printf("Input num1 (Int): \n");
+        String num1Str = scanner.nextLine();
+
+        System.out.printf("Input num2 (Int): \n");
+        String num2Str = scanner.nextLine();
+
+        try {
+            int num1 = Integer.parseInt(num1Str);
+            int num2 = Integer.parseInt(num2Str);
+
+            if (num1 > num2) {
+                System.out.println("num1 is greater: " +num1);
+            } else if (num1 < num2) {
+                System.out.println("num2 is greater: " +num2);
+            }
+            else {
+                System.out.println("They're the same");
+            }
+        } catch (NumberFormatException e) {
+            System.out.println("Invalid integer");
         }
     }
 }
